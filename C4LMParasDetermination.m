18b2773049fe8745a6bc8cce0df093ea167928f1
Jiayi Wang <Jiayi Wang>
2017-04-27 12:02:43 +0800

    Create matlab code for determining the design parameters of the compliant four-bar linkage mechanism

---
+++ C4LMParasDetermination.m
@@ -0,0 +1,16 @@
+%   Name: C4LMParasDetermination
+%   Description: Matlab Code for determining the parameters of the
+%   compliant four-bar linkage mechanism. Use lsqnonlin (non-linear least
+%   square algorithm) to fit the paratemeters of C4LM to meet the required
+%   torque profile
+%   Author: Jiayi Wang
+%   Date: 27/04/2017
+
+%% Determine parameters of torque profiles (a1, a1), given desired sfiffness
+
+
+
+%% Generate desired torque profile
+
+%% fit parameters of C4LM
+

28ffb831a0f043e0215a35dd1498d55f2c55c578
Jiayi Wang <Jiayi Wang>
2017-04-27 16:17:17 +0800

    Add derivation steps for determining the parameters of the torque profile Add calculations of the parameters of the torque profile in C4LMParasDetermination.m

--- C4LMParasDetermination.m
+++ C4LMParasDetermination.m
@@ -6,8 +6,39 @@
 %   Author: Jiayi Wang
 %   Date: 27/04/2017
 
+%% Clean workspace
+clc;
+clear;
+
 %% Determine parameters of torque profiles (a1, a1), given desired sfiffness
 
+%Define disired stiffness range(N*m/rad), format: [lower_bound upper_bound]
+StiffnessRange = [0.3 120];
+disp(['Stiffness Range: ',num2str(StiffnessRange(1)),' to ',num2str(StiffnessRange(2)),' Nm/rad']);
+disp(' ');
+
+%Define maximum positive and negative deflection angle of the C4LM
+%format:[lower_bound upper_bound]
+%   Deflection Angle (epsilon): the angle between the C4LM and shaft (The relative 
+%   displacement between the C4LM and the shaft, not the one between the link 
+%   and desired position of the joint)
+DeflectionAngle = [-pi/6 pi/6];
+disp(['Maximum C4LM Deflection Angle Range: ',num2str(DeflectionAngle(1)),' to ',num2str(DeflectionAngle(2)),' rad']);
+disp(' ');
+
+%Map the stiffness range to the deflection angle range
+%   Minimum and Maximum stiffness achieved at both two C4LM components reach maximum
+%   negative and postive deflection, respectively
+EpsilonMin = DeflectionAngle(1);
+EpsilonMax = DeflectionAngle(2);
+StiffnessMin = StiffnessRange(1);
+StiffnessMax = StiffnessRange(2);
+
+a2 = 1/4*(StiffnessMax-StiffnessMin)/(EpsilonMax-EpsilonMin);
+a1 = 1/2*StiffnessMax-2*a2*EpsilonMax;
+
+disp(['a2 = ',num2str(a2)],' Nm/rad^2');
+disp(['a1 = ',num2str(a1)],' Nm/rad');
 
 
 %% Generate desired torque profile

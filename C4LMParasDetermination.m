c7ec92a8fc8a6ba6ab0272ecba45c6b051394c57
Jiayi Wang <Jiayi Wang>
2017-04-28 18:55:18 +0800

    Add a function to calculate C4LM torques given different deflection situations
    Solved sync problems (rename derivation image)

--- C4LMParasDetermination.m
+++ C4LMParasDetermination.m
@@ -39,6 +39,7 @@ a1 = 1/2*StiffnessMax-2*a2*epsilonMax;
 
 disp(['a2 = ',num2str(a2),' Nm/rad^2']);
 disp(['a1 = ',num2str(a1),' Nm/rad']);
+disp(' ');
 
 
 %% Generate and plot torque profile determined by a2 and a1
@@ -46,11 +47,17 @@ epsilons = epsilonMin:1e-4:epsilonMax;
 TorquesProfile = a2*epsilons.^2+a1*epsilons; %TorqueProfile of a single compiant transmission element
 
 plot(epsilons,TorquesProfile)
-title(['Torque-Deflection Profile a_{1} = ', num2str(a1), ', a_{2} = ',num2str(a2)])
+title(['Torque-Deflection Profile for a Single Compliant Transmission Element(CTE) a_{1} = ', num2str(a1), ', a_{2} = ',num2str(a2)])
 xlabel('Deflection of the Entire Compliant Transmission Element {\epsilon} (rad)')
 ylabel('Torques {\tau} (Nm)')
 
 %% fit parameters of C4LM
-%Parameters need to be fitted: alpha_0,r1,r2,r3,r4,k2,k3,k4
+n = 3; %define numbers of C4LM in a single compliant transimission element
+disp(['Number of C4LM in a Single CTE(Compliant Transmission Element): ',num2str(n)])
 
+refTorque = TorquesProfile/n; %y: desired torque profile for a single four-bar linkage mechanism
 
+%Parameters need to be fitted: alpha0,r1,r2,r3,r4,k2,k3,k4
+%initial guess of variables to be optimized
+alpha0 = 1.0; r1 = 1.0; r2 = 1.0; r3 = 1.0; r4 = 1.0; k2 = 1.0; k3 = 1.0; k4 = 1.0;
+x0 = [alpha0, r1, r2, r3, r4, k2, k3, k4]; %put into vector form
